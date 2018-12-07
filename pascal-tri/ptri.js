a64a2f52476a1ec20caa1aab3bc4691ff0d52805
Guy John <guy@rumblesan.com>
2018-12-07 22:11:57 +0000

    Smaller pascals triangle

--- pascal-tri/ptri.js
+++ pascal-tri/ptri.js
@@ -1,1 +1,8 @@
-for(o=[],i=0;i++<process.argv[2];console.log((o=r).join(' ')))for(r=[],j=i;j--;)r[j]=o[j-1]+o[j]||1
+const arguments=[]
+for (i=2; j=process.argv[i]; i++) {
+  arguments[i-2]=''+j;
+}
+print=console.log
+// above in place for code-golf.io
+
+for(i=0;i++<20;print(o))for(o='',s=j=1;j<=i;s=s*(i-j)/j++)o+=s+' '
